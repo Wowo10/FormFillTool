40c2910496bb72fb4ad3f2916b8b98fc0d8cc60b
Wowo10 <wojtekwojtek529@gmail.com>
2021-01-14 00:53:26 +0100

    Added dummy context menu entry.

--- src/background.ts
+++ src/background.ts
@@ -1,3 +1,13 @@
-chrome.runtime.onInstalled.addListener(function () {
-	
+// chrome.runtime.onInstalled.addListener(function () {
+
+// });
+
+chrome.contextMenus.removeAll();
+chrome.contextMenus.create({
+    id: '1',
+    title: "FillForm",
+    contexts: ["all"]
+});
+chrome.contextMenus.onClicked.addListener((d) => {
+    console.log(d);
 });
